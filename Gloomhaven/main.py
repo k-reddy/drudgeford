80d6b38a9cc3f5c2527cdb7eb384ac243aaf366d
k-reddy <reddypkeerthi@gmail.com>
2024-09-25 11:24:55 -0700

    switching to python files from jupyter

--- Gloomhaven/main.py
+++ Gloomhaven/main.py
@@ -1,7 +1,18 @@
-import random
-from IPython.display import display, clear_output
-import sys
-import time
-import numpy as np
+import helpers
+import character
+import board
+import os
 
-board = Board(5)
+
+def main():
+    if os.getenv('TERM') is None:
+        os.environ['TERM'] = 'xterm'
+    player_name = input("What's your character's name? ")
+    helpers.clear_terminal()
+    monster = character.Character("Tree Man", 10, False, [2, 3])
+    player = character.Character(player_name, 10, True, [0, 0])
+    board.Board(5, monster, player)
+
+
+if __name__ == "__main__":
+    main()
