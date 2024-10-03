66f6b0d15f234f7630b78ceac55896c89f9f1312
Keerthi <reddypkeerthi@mgmail.com>
2024-10-03 14:43:38 -0700

    make display work for character class and replace the action card printing with display

--- Gloomhaven/display.py
+++ Gloomhaven/display.py
@@ -1,15 +1,24 @@
 class Display:
     def __init__(self) -> None:
-        log = ''
+        self.log = []
         pass
 
-    def draw_board(self):
+    def reload_display(self) -> None:
+        self.draw_board()
+        self.print_log()
+
+    def draw_board(self) -> None:
         pass
 
-    def add_to_log(self):
-        pass
+    def add_to_log(self, log_str: str) -> None:
+        self.log.append(log_str)
 
-    def print_log(self):
-        pass
+    def print_log(self, num_lines = 10) -> None:
+        for line in self.log[-num_lines:]:
+            print(line)
 
-    
+    def log_action_cards(self, action_cards) -> None:
+        self.add_to_log("Your action cards are: ")
+        for i, action_card in enumerate(action_cards):
+            self.add_to_log(f"{i}: {action_card}")
+
