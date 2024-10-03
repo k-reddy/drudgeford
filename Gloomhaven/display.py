e1bbe6d2047e372496bb93bb43056c472beb27ce
Keerthi <reddypkeerthi@mgmail.com>
2024-10-03 14:10:40 -0700

    adding shell class for display

---
+++ Gloomhaven/display.py
@@ -0,0 +1,15 @@
+class Display:
+    def __init__(self) -> None:
+        log = ''
+        pass
+
+    def draw_board(self):
+        pass
+
+    def add_to_log(self):
+        pass
+
+    def print_log(self):
+        pass
+
+    
