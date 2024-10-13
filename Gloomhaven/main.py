a2f49a8994678511c3f2138774109c1ccb398a06
Keerthi <reddypkeerthi@mgmail.com>
2024-10-12 20:00:57 -0700

    refactoring main for readability

--- Gloomhaven/main.py
+++ Gloomhaven/main.py
@@ -9,7 +9,36 @@ def main():
     # set up terminal
     if os.getenv("TERM") is None:
         os.environ["TERM"] = "xterm"
+    
+    disp = display.Display()
+    # get user input and set up number of players and names
+    players = set_up_players(disp)
+    # if players want game help, display instructions
+    provide_help_if_desired(disp)
+    # set up monsters
+    monsters = set_up_monsters(len(players), disp)
+    
+    board = Board(10, monsters, players, disp)
+    game = GameLoop(board, disp)
+    game.start()
 
+def set_up_players(disp):
+    num_players = 1
+    players = []
+    emoji = ["🧙", "🕺", "🐣"]
+    default_names = ["Happy", "Glad", "Jolly"]
+
+    # get some user input before starting the game
+    num_players = int(disp.get_user_input("How many players are playing? Type 1, 2, or 3.", ["1", "2", "3"]))
+    for i in range(num_players):
+        player_name = disp.get_user_input(prompt=f"What's Player {i+1}'s character's name? ")
+        # default to happy :D
+        player_name = player_name if player_name != "" else default_names[i]
+        players.append(character.Player(player_name, 10, disp, emoji[i]))
+    disp.clear_display()
+    return players
+
+def provide_help_if_desired(disp):
     help_message = '''Welcome to the game! Here's how it works:
 - You and the monster will attack each other once per turn in a random order
 - You can only attack if you are within range of your enemy
@@ -20,29 +49,13 @@ def main():
 
 Good luck!'''
 
-    
-    disp = display.Display()
-    num_players = 1
-    players = []
-    emoji = ["🧙", "🕺", "🐣"]
-    default_names = ["Happy", "Glad", "Jolly"]
-
-    # get some user input before starting the game
-    multi_player = True if disp.get_user_input("Would you like to play multi-player? (y)es or (n)o ", ["y", "n"]) == "y" else False
-    if multi_player:
-        num_players = int(disp.get_user_input("How many players? Type 1, 2, or 3.", ["1", "2", "3"]))
-    for i in range(num_players):
-        player_name = disp.get_user_input(prompt=f"What's Player {i+1}'s character's name? ")
-        # default to happy :D
-        player_name = player_name if player_name != "" else default_names[i]
-        players.append(character.Player(player_name, 10, disp, emoji[i]))
-    disp.clear_display()
     want_help = disp.get_user_input(prompt="Hit enter to start or type help for instructions ")
     if want_help == "help":
         disp.clear_display_and_print_message(help_message)
         disp.get_user_input(prompt="Hit enter to continue")
         disp.clear_display()
 
+def set_up_monsters(num_players, disp):
     monsters = []
     names = ["Tree Man", "Evil Blob", "Living Skeleton", "Evil Eye"]
     emoji = ["🌵", "🪼 ", "💀", "🧿"]
@@ -50,10 +63,6 @@ Good luck!'''
     for i in range(num_players+1):
         monster = character.Monster(names[i], healths[i], disp, emoji[i])
         monsters.append(monster)
-    board = Board(10, monsters, players, disp)
-    game = GameLoop(board, disp)
-    game.start()
-
-
+    return monsters
 if __name__ == "__main__":
     main()
