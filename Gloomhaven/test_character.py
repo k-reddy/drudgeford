c69b364af18b241312cd97da3a69098e845456a4
k-reddy <reddypkeerthi@gmail.com>
2024-10-15 17:36:26 -0700

    fixing test_human_perform_movement and for ai, now all tests passing

--- Gloomhaven/test_character.py
+++ Gloomhaven/test_character.py
@@ -56,7 +56,6 @@ def test_ai_select_attack_target():
 # !!! all these tests feel really messy - lots of failure points unrelated to what we're testing
 # !!! they also just check the most base cases - but doing otherwise feels like it'd be a ton of work
 def test_ai_perform_movement():
-    print(ai_monsters, ai_players)
     action_card = ActionCard(
         attack_name="test",
         strength=1,
@@ -69,16 +68,25 @@ def test_ai_perform_movement():
             ai_board.update_locations(i,j,None)
     
     # put the characters in known, clear locations
-    ai_board.update_character_location(
-        ai_monsters[0],
-        ai_board.find_location_of_target(ai_monsters[0]),
-        [0,0]
-    )
-    ai_board.update_character_location(
-        ai_players[0],
-        ai_board.find_location_of_target(ai_players[0]),
-        [2,2]
-    )
+    try:
+        ai_board.update_character_location(
+            ai_monsters[0],
+            ai_board.find_location_of_target(ai_monsters[0]),
+            [0,0]
+        )
+    except ValueError:
+        ai_board.update_locations(0,0,ai_monsters[0])
+    
+    try: 
+        ai_board.update_character_location(
+            ai_players[0],
+            ai_board.find_location_of_target(ai_players[0]),
+            [2,2]
+        )
+    except ValueError:
+        ai_board.update_location(
+            2,2, ai_players[0]
+        )
     # move
     ai_monsters[0].perform_movement(action_card, ai_board)
     # check new loc
@@ -87,3 +95,42 @@ def test_ai_perform_movement():
     ai_monsters[0].perform_movement(action_card, ai_board)
     # check new loc - shouldn't move
     assert ai_board.find_location_of_target(ai_monsters[0]) == (1,1)
+
+def test_human_perform_movement():
+    action_card = ActionCard(
+        attack_name="test",
+        strength=1,
+        distance=2,
+        movement=1
+    )
+    # clear a spot on the board
+    for i in range(2):
+        for j in range(2):
+            human_board.update_locations(i,j,None)
+    
+    # put the characters in known, clear locations - if we cleared them, have to reinsert them
+    try:
+        human_board.update_character_location(
+            human_players[0],
+            human_board.find_location_of_target(human_players[0]),
+            [1,1]
+        )
+    except ValueError:
+        human_board.update_locations(1,1,human_players[0])
+    # move
+    with patch("display.Display.get_user_input", return_value="q"):
+        human_players[0].perform_movement(action_card, human_board)
+    # check new loc
+    assert human_board.find_location_of_target(human_players[0]) == (0,0)
+
+    # move
+    with patch("display.Display.get_user_input", return_value="c"):
+        human_players[0].perform_movement(action_card, human_board)
+    # check new loc
+    assert human_board.find_location_of_target(human_players[0]) == (1,1)
+
+    # move
+    with patch("display.Display.get_user_input", return_value="s"):
+        human_players[0].perform_movement(action_card, human_board)
+    # check new loc
+    assert human_board.find_location_of_target(human_players[0]) == (2,1)
