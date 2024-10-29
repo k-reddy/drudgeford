6c70d5a1fda51e4982736bad9eb5a3246c3c535d
Keerthi <reddypkeerthi@mgmail.com>
2024-10-29 15:04:37 -0700

    fixing mushroom bug

--- Gloomhaven/board.py
+++ Gloomhaven/board.py
@@ -450,7 +450,7 @@ class Board:
             return TRAP_DAMAGE
         elif el == 'TOXIC_MUSHROOM':
             self.terrain[row][col] = 'X'
-            self.add_effect_to_terrain_for_attack("SPORE", row, col, 1, 4)
+            self.add_effect_to_terrain_for_attack("SPORE", row, col, shapes.circle(1), 4)
             return
         elif el == 'SPORE':
             return SPORE_DAMAGE
