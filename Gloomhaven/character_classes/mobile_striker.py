d98b9da45865eb08c53351dbc1bf5dfe879110bf
Keerthi <reddypkeerthi@mgmail.com>
2024-10-30 15:57:56 -0700

    making pseudocode files for enemy attacks

---
+++ Gloomhaven/character_classes/mobile_striker.py
@@ -0,0 +1,4 @@
+# Quick Strike - Attack 2 range 1, move 3
+# Flanking Strike - Attack 3 range 1, strengthen if attacking from behind, move 2
+# Basic Shot - Attack 2 range 3, move 1
+# Trap Line - Create line shape length 2 that applies curse to enemies who cross it, move 2
