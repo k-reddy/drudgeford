199bd6b78811c87a58d06b3ecb48ce9f168861cc
Keerthi <reddypkeerthi@mgmail.com>
2024-10-30 15:47:46 -0700

    adding pseudocode for all the character classes

--- Gloomhaven/character_classes/miner.py
+++ Gloomhaven/character_classes/miner.py
@@ -3,8 +3,15 @@ import attack_shapes as shapes
 
 
 cards = [
-    # 
-
+# Desparate Mining - Range 2, examine stone pile. If gems found, gain shield 2, bless, heal 2. If no gems, gain weaken. Move 1 
+# Heavy Hammer - Attack 4, range 1, no movement, charge next attack +2
+# Pickaxe Sweep - Attack 2, hits in arc pattern, move 2
+# Mason's Shield - Shield 1, attack 4
+# Stone Meditation - Skip turn, heal 4 if adjacent to stone pile, shield 2
+# Tax Assessment - Weaken target, pull 2, attack 3
+# Crystal Placebo - Attack 2, range 1, heal 4
+# Stone Foundation - Create impassable stone barrier length 2, shield all adjacent allies
+# Self Sacrifice - Attack 6, range 1, bless self but -3 health, move 1
 ]
 backstory = ""
-health = 8
+health = 16
