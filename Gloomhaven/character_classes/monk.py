b2e1e800956788a725872c3caa377552044e0680
k-reddy <reddypkeerthi@gmail.com>
2024-11-03 20:32:56 -0800

    added all monk attacks

--- Gloomhaven/character_classes/monk.py
+++ Gloomhaven/character_classes/monk.py
@@ -14,18 +14,65 @@ cards = [
     action_model.ActionCard(
         attack_name="Sacred Cultivation",
         actions=[
-            action_model.ModifySelfHealth(3),
-            action_model.BlessSelf(),
+            action_model.HealAlly(strength=4, att_range=2),
+            action_model.BlessAndChargeAlly(att_range=2, strength=2),
         ],
         movement=3,
         jump=False
+    ),
+    action_model.ActionCard(
+        attack_name="Estrangement",
+        actions=[
+            action_model.Push(3,2),
+            action_model.BlessSelf(),
+        ],
+        movement=2,
+        jump=False
+    ),
+    action_model.ActionCard(
+        attack_name="Fermented Fury",
+        actions=[
+            action_model.SingleTargetAttack(2,1),
+            action_model.Curse(1),
+            action_model.ChargeNextAttack(2),
+        ],
+        movement=1,
+        jump=False
+    ),
+    action_model.ActionCard(
+        attack_name="Seeds of Wrath",
+        actions=[
+            action_model.BlessAndChargeAlly(2,3),
+        ],
+        movement=2,
+        jump=False
+    ),
+    action_model.ActionCard(
+        attack_name="Scythe Swipe",
+        actions=[
+            action_model.SingleTargetAttack(4,1)
+        ],
+        movement=2,
+        jump=False
+    ),
+    action_model.ActionCard(
+        attack_name="Field Runner",
+        actions=[
+            action_model.ModifySelfHealth(2),
+            action_model.SingleTargetAttack(2,1)
+        ],
+        movement=4,
+        jump=False
+    ),
+    action_model.ActionCard(
+        attack_name="Prayer of Spite",
+        actions=[
+            action_model.SingleTargetAttack(6,2),
+            action_model.CurseSelf()
+        ],
+        movement=0,
+        jump=False
     )
 ]
-# Estrangement - Push 3, bless self, move 2
-# Fermented Fury - Attack 2 range 1, curse target, strengthen self, move 1
-# Seeds of Wrath - Bless ally, charge next attack +3, move 2
-# Scythe Swipe - Attack 4 range 0, move 1, arc pattern 
-# Field Runner - Heal 2, move 4, strengthen if you end next to ally
-# Prayer of Spite - Attack 6 range 2, curse self, move 0
 health = 12
 backstory = "I'm a monk. Hiya!"
--- Gloomhaven/game_loop.py
+++ Gloomhaven/game_loop.py
@@ -209,7 +209,7 @@ Kill it or be killed..."""
         players = []
         emoji = ["🧙", "🕺", "🐣"]
         default_names = ["Happy", "Glad", "Jolly"]
-        char_classes = [character.Necromancer, character.Miner, character.Wizard, ]
+        char_classes = [character.Monk, character.Necromancer, character.Miner, character.Wizard, ]
 
         # get some user input before starting the game
         num_players = (
