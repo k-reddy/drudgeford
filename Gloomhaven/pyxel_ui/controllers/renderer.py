398405f3b4d59004343688c637b8b713e3719a67
Keerthi <reddypkeerthi@mgmail.com>
2024-11-06 13:58:23 -0800

    working MapView in renderer

--- Gloomhaven/pyxel_ui/controllers/renderer.py
+++ Gloomhaven/pyxel_ui/controllers/renderer.py
@@ -13,7 +13,7 @@ from pyxel_ui.models.entity import Entity
 from pyxel_ui.models.font import PixelFont
 from pyxel_ui.utils import BACKGROUND_TILES, draw_tile
 from pyxel_ui.views.sprite import Sprite, SpriteManager
-from pyxel_ui.models.view_section import LogView
+from pyxel_ui.models.view_section import LogView, MapView
 
 
 class Renderer:
@@ -81,89 +81,7 @@ class Renderer:
             pyxel.text(BITS * (end_idx - 1) * 4, indicator_y, "-> Next", col=7)
 
     # End card methods
-
-    def draw_background(
-        self,
-        floor_tile_keys: list[str],
-        valid_map_coordinates: list[tuple[int, int]],
-    ) -> None:
-        directions = [
-            {
-                "coord": (1, 0),
-                "wall_tile_name": "new_wall_ns",
-                "height": 32,
-                "width": 4,
-            },
-            {
-                "coord": (-1, 0),
-                "wall_tile_name": "new_wall_ns",
-                "height": 32,
-                "width": 4,
-            },
-            {
-                "coord": (0, 1),
-                "wall_tile_name": "new_wall_ew",
-                "height": 4,
-                "width": 32,
-            },
-            {
-                "coord": (0, -1),
-                "wall_tile_name": "new_wall_ew",
-                "height": 4,
-                "width": 32,
-            },
-        ]
-
-        # offset the map by wall thickness in x and y directions
-        # draw the floor tiles where they belong
-        # leave space for the walls and draw the floor
-        floor_start_x = self.canvas.board_start_pos[0]
-        floor_start_y = self.canvas.board_start_pos[1]
-
-        # ensure we get the same floor tiles each time we draw the floor
-        random.seed(100)
-
-        for x, y in valid_map_coordinates:
-            x_px = x * self.canvas.tile_width_px + floor_start_x
-            y_px = y * self.canvas.tile_height_px + floor_start_y
-            floor_tile = BACKGROUND_TILES[random.choice(floor_tile_keys)]
-
-            draw_tile(x_px, y_px, **floor_tile)
-        # come back through and draw the walls
-        for x, y in valid_map_coordinates:
-            x_px = x * self.canvas.tile_width_px + floor_start_x
-            y_px = y * self.canvas.tile_height_px + floor_start_y
-            for direction in directions:
-                new_coord = tuple(a + b for a, b in zip((x, y), direction["coord"]))
-                if new_coord in valid_map_coordinates:
-                    continue
-                x_px_new = (
-                    x_px + self.canvas.tile_width_px * direction["coord"][0]
-                    if direction["coord"][0] > 0
-                    else x_px + direction["width"] * direction["coord"][0]
-                )
-                y_px_new = (
-                    y_px + self.canvas.tile_height_px * direction["coord"][1]
-                    if direction["coord"][1] > 0
-                    else y_px + direction["height"] * direction["coord"][1]
-                )
-                draw_tile(
-                    x_px_new,
-                    y_px_new,
-                    **BACKGROUND_TILES[direction["wall_tile_name"]],
-                )
-
-    def draw_grid(self, valid_floor_coordinates: list[tuple[int, int]]) -> None:
-        # draw grid only on valid floor coordinates
-        for x, y in valid_floor_coordinates:
-            pyxel.rectb(
-                x * self.canvas.tile_width_px + self.canvas.board_start_pos[0],
-                y * self.canvas.tile_height_px + self.canvas.board_start_pos[1],
-                self.canvas.tile_width_px,
-                self.canvas.tile_height_px,
-                GRID_COLOR,
-            )
-
+    
     def draw_health_and_iniative_bar(
         self,
         sprite_names: list[str],
@@ -246,24 +164,18 @@ class Renderer:
         logview.round_number=round_number
         logview.acting_character_name=acting_character_name
         logview.draw()
-        # x = BITS * 11
-        # y = BITS
-        # if log or round_number > 0:
-        #     for line in [f"Round {round_number}, {acting_character_name}'s turn"] + log[
-        #         -MAX_LOG_LINES:
-        #     ]:
-        #         self.font.draw_text(
-        #             x, y, line, col=7, size="medium", max_width=BITS * 8
-        #         )
-        #         y += (
-        #             self.font.get_text_height(line, size="medium", max_width=BITS * 8)
-        #             + 4
-        #         )
+
 
     def draw_map(self, valid_floor_coordinates: list[tuple[int, int]]) -> None:
-        dungeon_floor_tiles = [f"dungeon_floor_cracked_{i}" for i in range(1, 13)]
-        self.draw_background(dungeon_floor_tiles, valid_floor_coordinates)
-        self.draw_grid(valid_floor_coordinates)
+        mapview = MapView(
+            self.font, 
+            self.canvas.board_start_pos,
+            [BITS*10, BITS*10]
+            )
+        mapview.tile_width_px = self.canvas.tile_width_px
+        mapview.tile_height_px = self.canvas.tile_height_px
+        mapview.valid_map_coordinates = valid_floor_coordinates
+        mapview.draw()
 
     def draw_sprite(self, x, y, sprite: Sprite, colkey=0, scale=1) -> None:
         # sprite = self.sprite_manager.get_sprite(sprite_name, animation_frame)
