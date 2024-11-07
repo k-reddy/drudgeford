657448043015491bda453262a25868129835fbfa
k-reddy <reddypkeerthi@gmail.com>
2024-11-06 16:37:13 -0800

    pushing responsibilities to view manager

--- Gloomhaven/pyxel_ui/controllers/view_manager.py
+++ Gloomhaven/pyxel_ui/controllers/view_manager.py
@@ -36,3 +36,20 @@ class ViewManager:
             self.font,
             [BITS*11, self.view_border],
             [pyxel.width, BITS*11])
+        
+    def update_log(
+            self, 
+            log: list[str]):
+        self.log_view.log = log
+
+    def update_round_turn(self, round_number: int, acting_character_name: str):
+        self.log_view.round_number = round_number
+        self.log_view.acting_character_name = acting_character_name
+
+    def update_initiative_bar(self, sprite_names: list[str], healths: list[int], teams: list[bool]):
+        self.initiative_bar_view.sprite_names = sprite_names
+        self.initiative_bar_view.healths = healths
+        self.initiative_bar_view.teams = teams
+
+    def update_action_card_log(self, action_card_log: list[str]):
+        self.action_card_view.action_card_log = action_card_log
