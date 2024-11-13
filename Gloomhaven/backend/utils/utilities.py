6e3a13b6591524d29884cb0d773f63c85fbe88ee
k-reddy <reddypkeerthi@gmail.com>
2024-11-13 13:27:35 -0800

    adding ability to save and load levels

--- Gloomhaven/backend/utils/utilities.py
+++ Gloomhaven/backend/utils/utilities.py
@@ -1,5 +1,6 @@
 from functools import partial
 from enum import Enum, auto
+from pathlib import Path
 
 def make_multiply_modifier(multiplier: int, multiplier_text: str) -> tuple:
     def multiply(x, y):
@@ -17,3 +18,6 @@ class GameState(Enum):
     WIN = auto()
     GAME_OVER = auto()
     EXHAUSTED = auto()
+
+def get_campaign_filenames():
+    return [p.name for p in list(Path(".").glob("*.pickle")) if "game_" in p.name]
