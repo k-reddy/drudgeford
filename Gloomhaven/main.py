07f2322e80a6574da5dfcf787d50b29c60b9ad92
Keerthi <reddypkeerthi@mgmail.com>
2024-11-21 19:01:42 -0800

    moving help message over to frontend

--- Gloomhaven/main.py
+++ Gloomhaven/main.py
@@ -40,9 +40,6 @@ def main(num_players: int = 1, all_ai_mode=False):
     if not all_ai_mode:
         disp.clear_display()
 
-    # if players want game help, display instructions
-    provide_help_if_desired(disp, all_ai_mode)
-
     # make a campaign
     campaign = Campaign(disp, num_players, all_ai_mode)
 
@@ -57,34 +54,6 @@ def main(num_players: int = 1, all_ai_mode=False):
         disp.get_user_input(prompt="Hit enter to continue")
         disp.clear_display()
     campaign.start_campaign()
-
-
-def provide_help_if_desired(disp, all_ai_mode):
-    help_message = [
-        "Welcome to the game! Here's how it works:",
-        "- All players are on the same team against the AI monsters - a green line under the character in the health bar denotes a player, red denotes a monster,",
-        "- The game is turn based, and the turn order is randomly determined each round (see the health bar for round order)",
-        "- Each turn, you will pick an attack card. Once you use a card, it's gone until you use all your cards, so choose wisely!",
-        "- Movement on the card is how far you can move, strength is how much damage your attack will do, and range is how many squares away you can be and still hit a monster (range 1 is adjacent)",
-        "- Every time you attack, you'll draw an 'attack modifier card.' This adds some randomness to your damage. Certain cards (bless, curse) add good/bad modifiers randomly to your deck, and others (charge attack) determine what the next modifier you draw will be",
-        "- Some cards place elements/obstacles on the map, like traps and fire. Most of these do damage.",
-        "- The exceptions are ice, which gives you a 25% chance of slipping and losing your turn when you step on it, and shadow, which gives all attacks a 10% chance of missing for each square of shadow they pass through",
-        "Good luck!"
-    ]
-    want_help = False
-    if not all_ai_mode:
-        user_input = disp.get_user_input(
-            prompt="Hit enter to start or type help for instructions "
-        )
-        if user_input == "help":
-            want_help = True
-    if want_help:
-        disp.print_message("", True)
-        for line in help_message:
-            disp.print_message(textwrap.fill(line, TEXT_WIDTH), False)
-            disp.print_message("", False)
-        disp.get_user_input(prompt="Hit enter to continue")
-        disp.clear_display()
 
 if __name__ == "__main__":
     main()
