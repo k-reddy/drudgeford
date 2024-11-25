7ae641c42bc50ae4c297cbd4d0497d6643d3a456
Keerthi <reddypkeerthi@mgmail.com>
2024-11-25 14:10:42 -0800

    some formatting and changing server and tcp client to work on the remote server

--- Gloomhaven/server/tcp_client.py
+++ Gloomhaven/server/tcp_client.py
@@ -7,7 +7,7 @@ class ClientType(Enum):
     FRONTEND = "frontend"
 
 class TCPClient:
-    def __init__(self, client_type: ClientType, host='localhost', port=8080):
+    def __init__(self, client_type: ClientType, host="13.59.128.25", port=8080):
         self.client_type = client_type
         self.client_id = None
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
