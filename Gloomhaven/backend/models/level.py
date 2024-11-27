ef3f99ca68e93f93a8e037fb68a5e3eb774d8b07
k-reddy <reddypkeerthi@gmail.com>
2024-11-27 13:15:56 -0500

    restoring levels file to the proper starting point

--- Gloomhaven/backend/models/level.py
+++ Gloomhaven/backend/models/level.py
@@ -20,21 +20,26 @@ class Level:
 
 
 campaign_levels = [
-    # Level(
-    #     floor_color_map=[(1,3), (5,11)],
-    #     wall_color_map=[(1,4), (13,15)],
-    #     monster_classes=[character.Treeman, character.MushroomMan, character.Fairy],
-    #     pre_level_text=textwrap.fill('''A hunter you talked to at the tavern mentioned seeing the same mysterious rune that's been appearing on village doors carved into the bark of an ancient tree in a forest nearby. When the village healer mentioned that the mushrooms from that part of the forest had suddenly lost their curative effects, you decide to investigate the area. The rune on the tree points to a trail of withered vegetation. You follow it deep into the forest - only to find yourself surrounded by forest creatures whose vacant eyes and jerky movements suggest they're under some dark influence. Before you can retreat, they lunge at you in perfect, unsettling union.''',
-    #     TEXT_WIDTH),
-    #     starting_elements=[obstacle.Spores, obstacle.PoisonShroom]),
-    # Level(
-    #     floor_color_map=[(1,8), (5,2)],
-    #     wall_color_map=[],# (1,2), (13,14)
-    #     monster_classes=[character.Demon, character.Fiend, character.FireSprite],
-    #     pre_level_text=textwrap.fill('''As you kill the last of the corrupted creatures, a dark figure in a hooded robe materializes at the forest's edge. Before you can focus on it, the shape dissolves into shadow. Your head erupts into searing pain, as a raspy voice booms in your mind. 'I have you in my grasp now,' it cackles. 'Good luck ever escaping me.' The world spins, then fades to black as you collapse. You wake up burning hot, and your eyes open to the sight of demons and fire - you'll need to fight through them to survive.''',
-    #     TEXT_WIDTH),
-    #     starting_elements=[obstacle.Fire, obstacle.Trap, obstacle.Shadow]
-    #     ),
+    Level(
+        floor_color_map=[(1, 3), (5, 11)],
+        wall_color_map=[(1, 4), (13, 15)],
+        monster_classes=[character.Treeman, character.MushroomMan, character.Fairy],
+        pre_level_text=textwrap.fill(
+            """A hunter you talked to at the tavern mentioned seeing the same mysterious rune that's been appearing on village doors carved into the bark of an ancient tree in a forest nearby. When the village healer mentioned that the mushrooms from that part of the forest had suddenly lost their curative effects, you decide to investigate the area. The rune on the tree points to a trail of withered vegetation. You follow it deep into the forest - only to find yourself surrounded by forest creatures whose vacant eyes and jerky movements suggest they're under some dark influence. Before you can retreat, they lunge at you in perfect, unsettling union.""",
+            TEXT_WIDTH,
+        ),
+        starting_elements=[obstacle.Spores, obstacle.PoisonShroom],
+    ),
+    Level(
+        floor_color_map=[(1, 8), (5, 2)],
+        wall_color_map=[],  # (1,2), (13,14)
+        monster_classes=[character.Demon, character.Fiend, character.FireSprite],
+        pre_level_text=textwrap.fill(
+            """As you kill the last of the corrupted creatures, a dark figure in a hooded robe materializes at the forest's edge. Before you can focus on it, the shape dissolves into shadow. Your head erupts into searing pain, as a raspy voice booms in your mind. 'I have you in my grasp now,' it cackles. 'Good luck ever escaping me.' The world spins, then fades to black as you collapse. You wake up burning hot, and your eyes open to the sight of demons and fire - you'll need to fight through them to survive.""",
+            TEXT_WIDTH,
+        ),
+        starting_elements=[obstacle.Fire, obstacle.Trap, obstacle.Shadow],
+    ),
     Level(
         floor_color_map=[(1, 6), (5, 7)],
         wall_color_map=[(13, 12)],
