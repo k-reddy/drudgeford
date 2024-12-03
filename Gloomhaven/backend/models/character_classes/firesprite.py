a1f53a0afd3d93dee3e703ffeaa1e6f2d18964a3
k-reddy <reddypkeerthi@gmail.com>
2024-12-02 16:55:50 -0800

    trying out some difficulty changes for the first few levels

--- Gloomhaven/backend/models/character_classes/firesprite.py
+++ Gloomhaven/backend/models/character_classes/firesprite.py
@@ -10,19 +10,18 @@ cards = [
             actions.SingleTargetAttack(2, 4),
         ],
         movement=3,
-        jump=True
+        jump=True,
     ),
     actions.ActionCard(
         attack_name="Fire Trail",
         actions=[
             actions.ElementAreaEffectFromSelf(
-                shape=shapes.line((1,0),4),
-                element_type=obstacle.Fire
+                shape=shapes.line((1, 0), 4), element_type=obstacle.Fire
             ),
-            actions.WeakenAllEnemies(1, 2)
+            actions.WeakenAllEnemies(1, 2),
         ],
         movement=4,
-        jump=True
+        jump=True,
     ),
     actions.ActionCard(
         attack_name="Spark Storm",
@@ -30,41 +29,39 @@ cards = [
             actions.AreaAttack(shape=shapes.circle(1), strength=2),
         ],
         movement=3,
-        jump=True
+        jump=True,
     ),
     actions.ActionCard(
         attack_name="Burning Dash",
         actions=[
             actions.ElementAreaEffectFromSelf(
-                shape=shapes.cone(2),
-                element_type=obstacle.Fire
+                shape=shapes.cone(2), element_type=obstacle.Fire
             ),
-            actions.AreaAttack(shape=shapes.cone(2), strength=2)
+            actions.AreaAttack(shape=shapes.cone(2), strength=2),
         ],
         movement=5,  # Increased base movement to represent the "dash"
-        jump=True
+        jump=True,
     ),
     actions.ActionCard(
         attack_name="Ember Shield",
         actions=[
             actions.ShieldAllAllies(1, 2, 2),
             actions.ElementAreaEffectFromSelf(
-                shape=shapes.ring(2),
-                element_type=obstacle.Fire
-            )
+                shape=shapes.ring(2), element_type=obstacle.Fire
+            ),
         ],
         movement=3,
-        jump=True
+        jump=True,
     ),
     actions.ActionCard(
         attack_name="Heat Wave",
         actions=[
             actions.WeakenAllEnemies(2, 2),
-            actions.AreaAttack(shapes.circle(2),2)
+            actions.AreaAttack(shapes.circle(2), 2),
         ],
         movement=3,
-        jump=True
-    )
+        jump=True,
+    ),
 ]
 
-health = 3
+health = 4
