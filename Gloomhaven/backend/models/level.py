3549e6799b44df6cc9c6ec29a224116cf749e7bc
Keerthi <reddypkeerthi@mgmail.com>
2024-12-05 18:28:26 -0800

    moving kill target below logging so we can see the damage if you kill someone

--- Gloomhaven/backend/models/level.py
+++ Gloomhaven/backend/models/level.py
@@ -29,7 +29,12 @@ campaign_levels = [
         wall_color_map=[],  # (1,2), (13,14)
         monster_classes=[character.Demon, character.Fiend, character.FireSprite],
         pre_level_text="""As you kill the last of the corrupted creatures, a dark figure in a hooded robe materializes at the forest's edge. Before you can focus on it, the shape dissolves into shadow. Your head erupts into searing pain, as a raspy voice booms in your mind. 'I have you in my grasp now,' it cackles. 'Good luck ever escaping me.' The world spins, then fades to black as you collapse. You wake up burning hot, and your eyes open to the sight of demons and fire - you'll need to fight through them to survive.""",
-        starting_elements=[obstacle.Fire, obstacle.Trap, obstacle.Shadow],
+        starting_elements=[
+            obstacle.Fire,
+            obstacle.Fire,
+            obstacle.Trap,
+            obstacle.Shadow,
+        ],
     ),
     Level(
         floor_color_map=[(1, 6), (5, 7)],
